05c17ce31df1e2a6fbaa1de93144a60853f657ec
Hiroaki Takeda <henry_hiroaki@hotmail.com>
2021-07-30 01:43:52 -0700

    week4

--- data/init.sql
+++ data/init.sql
@@ -1,14 +1,13 @@
-Create schema french_words;
-
-CREATE TABLE lexicon (
-    id int not null AUTO_INCREMENT,
-    form CHARACTER(255),
-    pos character(3),
-    gender character(1),
-    verb_class character(3),
-    adj_f CHARACTER(255),
-    adj_p CHARACTER(255),
-    adj_fp CHARACTER(255),
-    definition CHARACTER(255),
-    origin_form CHARACTER(255)
-    );
+CREATE SCHEMA french_words;
+    CREATE TABLE french_words.lexicon (
+        id int not null AUTO_INCREMENT,
+        form CHARACTER(255),
+        pos character(3),
+        gender character(1),
+        verb_class character(3),
+        adj_f CHARACTER(255),
+        adj_p CHARACTER(255),
+        adj_fp CHARACTER(255),
+        definition CHARACTER(255),
+        origin_form CHARACTER(255)
+        );
